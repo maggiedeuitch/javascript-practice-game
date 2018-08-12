466b2da7110bb85875ea3dd4790f296a133bda81
maggiedeuitch <maggiedeuitch@gmail.com>
2018-08-12 18:40:01 -0400

    added enemyList object and for in loop for enemyList

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,60 +1,87 @@
 console.log("linked");
 
-    // <script>
-        const ctx = document.getElementById("ctx").getContext("2d");
-        ctx.font = '30px Arial';
-        const height = 500;
-        const width = 500;
-        // var message = 'Bouncing';
-        
-        let player = {
-            x : 50,
-            spdX : 30,
-            y : 40,
-            spdY : 5,
-            name : "P",
-        };
+// <script>
+const ctx = document.getElementById("ctx").getContext("2d");
+ctx.font = '30px Arial';
+const height = 500;
+const width = 500;
+// var message = 'Bouncing';
 
-        let enemy = {
-            x : 150,
-            spdX : 10,
-            y : 350,
-            spdY : 15,
-            name : "E"
-        };
+const player = {
+    x : 50,
+    spdX : 30,
+    y : 40,
+    spdY : 5,
+    name : "P"
+};
+//enemyList stores all the enemies in an object
+let enemyList = {};
 
-        let enemy2 = {
-            x : 250,
-            spdX : 10,
-            y : 450,
-            spdY : 15,
-            name : "E"
-        };
+const enemy = {
+    x : 150,
+    spdX : 10,
+    y : 350,
+    spdY : 15,
+    name : "E",
+    id: "E1"
+};
+//create the first item in enemyList
+enemyList["E1"] = enemy;
 
-        setInterval(update, 40);
-        
-        function updateEntity (entity) {
-        //entity movement
-            entity.x += entity.spdX;
-            entity.y += entity.spdY;
-            ctx.fillText(entity.name, entity.x, entity.y);
-            // console.log(`${entity} moving`, x);
-        //collision detection to change the direction of entity when it hits the boundaries
-            if(entity.x < 0 || entity.x > width) {
-                console.log('Out of Bounds');
-                entity.spdX = -entity.spdX;
-            }
-            if(entity.y < 0 || entity.y > height) {
-                entity.spdY = -entity.spdY;
-            }
-        }
+const enemy2 = {
+    x : 250,
+    spdX : 10,
+    y : 450,
+    spdY : 15,
+    name : "E",
+    id: "E2"
+};
 
-    function update() {
-        updateEntity(player);
-        updateEntity(enemy);
-        updateEntity(enemy2);
+enemyList["E2"] = enemy2;
+console.log(enemyList);
 
-    };
+const enemy3 = {
+    x : 230,
+    spdX : 5,
+    y : 70,
+    spdY : -8,
+    name : "E",
+    id: "E3"
+};
+
+enemyList["E3"] = enemy3;
+console.log(enemyList);
+
+setInterval(update, 40);
+
+function updateEntity (entity) {
+//entity movement
+    entity.x += entity.spdX;
+    entity.y += entity.spdY;
+    ctx.fillText(entity.name, entity.x, entity.y);
+    // console.log(`${entity} moving`, x);
+//collision detection to change the direction of entity when it hits the boundaries
+    if(entity.x < 0 || entity.x > width) {
+        // console.log('Out of Bounds');
+        entity.spdX = -entity.spdX;
+    }
+    if(entity.y < 0 || entity.y > height) {
+        entity.spdY = -entity.spdY;
+    }
+}
+
+function update() {
+// clears rectangle in canvas so the fillText doesn't just repeat
+    ctx.clearRect(0, 0, width, height);
+
+    updateEntity(player);
+
+    //loop through enemyList to update enemies
+    for (let key in enemyList) {
+        updateEntity(enemyList[key]);
+    }
+
+};
 
 
 
